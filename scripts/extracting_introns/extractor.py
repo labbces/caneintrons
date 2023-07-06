4a3c0832c760d5aeeff002e8f3a5dfb0e900c87e
beatriz <bia.estevam.25@usp.br>
2023-07-06 09:09:07 +0200

    update extractor

--- scripts/extracting_introns/extractor.py
+++ scripts/extracting_introns/extractor.py
@@ -1,12 +1,3 @@
-# add argparse para os caminhos dos arquivos.
-# mudar os gtf para ter os meso identificadores que o fasta
-# python3 -u "/home/bia/sugarcane_introns_local/extracting and trimming/\
-# extractor.py" -i /home/bia/sugarcane_introns_local/caneintrons/scripts/\
-# trimming_gff_files/trimmed_gtf/SP803280_D_151-161.gtf -f /home/bia/\
-# sugarcane_introns/introns_sugarcane/data/Genomes/GCA_002018215.1_CTBE_SP803280_v1.0_genomic.fna\
-#  -o output -c /home/bia/sugarcane_introns_local/spliceator_anaysis/\
-# contig_size_data.csv
-
 from Bio import SeqIO
 from Bio.Seq import Seq
 import pyranges as pr
@@ -20,6 +11,8 @@ parser.add_argument("-f", "--path_to_fasta", type=str, required=True)
 parser.add_argument("-o", "--output", type=str, required=True)
 parser.add_argument("-c", "--contig_table", type=str, required=True,
                     help='table with contig IDs and their lengths')
+parser.add_argument("-t", "--trimming_type", choices=['IMEter', 'spliceator',
+                    'splice2deep'], help='', default='IMEter')
 # parser.add_argument("-x", "--fasta_index", type=str, required=True)
 args = parser.parse_args()
 
@@ -34,17 +27,7 @@ contig_table = args.contig_table
 contig_table_pd = pd.read_csv(contig_table, header=None)
 contig_lengths = dict(zip(contig_table_pd[0], contig_table_pd[1]))
 
-
-# Getting gtf file
-# gtf = pr.read_gtf('/home/bia/sugarcane_introns/gtf_teste_cana.gtf')
-# gtf = pr.read_gtf('/home/bia/sugarcane_introns/old_nov30/\
-# 2.TrimmingGenomeFile/SP803280_141-151.gtf')
 gtf = pr.read_gtf(args.gtf_input)
-
-# gtf basic operations
-# print(gtf)
-# print(gtf.describe("transcript_id"))
-# print(gtf.features.introns(by="transcript"))
 
 # Introns features pyranges and its basic operations
 introns = gtf.features.introns(by="transcript")
@@ -55,10 +38,13 @@ introns = gtf.features.introns(by="transcript")
 # Getting only introns that follows: 150 > intron size > 100
 introns = (introns[(introns.End - introns.Start) > 99])
 introns = (introns[(introns.End - introns.Start) < 151])
-print(introns)
-
+# print(introns)
 
 filename = args.output
+filename_donor = filename + "donor"
+filename_acceptor = filename + "acceptor"
+
+trimming_type = args.trimming_type
 
 with open(f'{filename}_intron.fa', 'w') as intron_file:
     for seq_id, Start, End, gene_id, strand in \
@@ -69,29 +55,86 @@ with open(f'{filename}_intron.fa', 'w') as intron_file:
         seq_full = fasta_index[seq_id].seq
 
         id_full = f'>{seq_id}.{Start}-{End}-{strand}-{gene_id}'
+        # id_full_donor = f'>{seq_id}.{Start}-{End}-{strand}-{gene_id}-donor'
+        # id_full_acceptor = f'>{seq_id}.{Start}-{End}-{strand}-{gene_id}-acceptor'
 
-        start = Start - 200
-        end = End + 200
+        if trimming_type == "splice2deep":
+            with open(f'{filename_donor}_intron.fa', 'w') as donor_intron_file:
+                with open(f'{filename_acceptor}_intron.fa', 'w') as acceptor_intron_file:
+                    # start = Start - 300
+                    donor_start = Start - 300
+                    acceptor_start = End - 302
 
-        if start <= 0:
-            start = 1
-            print(id_full)
+                    # end = End + 300
+                    donor_end = Start + 302
+                    acceptor_end = End + 300
 
-        if end >= contig_lengths[seq_id]:
-            end = contig_lengths[seq_id]
-            print(id_full)
+                    if donor_start <= 0 or acceptor_start <= 0:
+                        # start = 1
+                        print(
+                            f'Short start border on {id_full} {Start} {End} {contig_lengths[seq_id]}')
+                    else:
+                        if acceptor_end >= contig_lengths[seq_id] or donor_end >= contig_lengths[seq_id]:
+                            # end = contig_lengths[seq_id]
+                            print(f'Short end border on {id_full}')
+                        else:
+                            # intron_seq = seq_full[start:end]
+                            # donor_seq = seq_full[donor_start:donor_end]
+                            # acceptor_seq = seq_full[acceptor_start:acceptor_end]
 
-        intron_seq = seq_full[start:end]
+                            if strand == "+":
+                                #intron_seq = intron_seq
+                                donor_seq = seq_full[donor_start:donor_end].upper(
+                                )
+                                acceptor_seq = seq_full[acceptor_start:acceptor_end].upper(
+                                )
+                            elif strand == "-":
+                                acceptor_seq = seq_full[donor_start:donor_end].upper(
+                                )
+                                donor_seq = seq_full[acceptor_start:acceptor_end].upper(
+                                )
+                                donor_seq = Seq(donor_seq)
+                                donor_seq = donor_seq.reverse_complement()
+                                acceptor_seq = Seq(acceptor_seq)
+                                acceptor_seq = acceptor_seq.reverse_complement()
+                            else:
+                                print(
+                                    f'Strand {strand} is not + or - (gene {gene_id} and\
+                                        seq_id {seq_id}')
 
-        if strand == "+":
-            intron_seq = intron_seq
-        elif strand == "-":
-            seq = Seq(intron_seq)
-            intron_seq = seq.reverse_complement()
-        else:
-            print(
-                f'Strand {strand} is not + or - (gene {gene_id} and\
-                     seq_id {seq_id}')
+                            donor_intron_file.write(f'{id_full}\n')
+                            donor_intron_file.write(f'{donor_seq}\n')
 
-        intron_file.write(f'{id_full}\n')
-        intron_file.write(f'{intron_seq}\n')
+                            acceptor_intron_file.write(f'{id_full}\n')
+                            acceptor_intron_file.write(f'{acceptor_seq}\n')
+                            #print(f"\n{id_full} \n{donor_seq}\n{acceptor_seq}\n")
+        elif trimming_type == "IMEter":
+            # start = Start - 300
+            intron_start = Start
+            intron_end = End
+
+            if intron_start <= 0:
+                print(
+                    f'Short start border on {id_full} {Start} {End} {contig_lengths[seq_id]}')
+            else:
+                if intron_end >= contig_lengths[seq_id]:
+                    # end = contig_lengths[seq_id]
+                    print(f'Short end border on {id_full}')
+                else:
+                    # intron_seq = seq_full[start:end]
+                    # donor_seq = seq_full[donor_start:donor_end]
+                    # acceptor_seq = seq_full[acceptor_start:acceptor_end]
+
+                    intron_seq = seq_full[intron_start:intron_end].upper()
+
+                    if strand == "-":
+                        intron_seq = Seq(intron_seq)
+                        intron_seq = intron_seq.reverse_complement()
+                    elif strand != '+':
+                        print(
+                            f'Strand {strand} is not + or - (gene {gene_id} and\
+                                seq_id {seq_id}')
+
+                    intron_file.write(f'{id_full}\n')
+                    intron_file.write(f'{intron_seq}\n')
+                    #print(f"\n{id_full} \n{donor_seq}\n{acceptor_seq}\n")
