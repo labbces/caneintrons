ee11feb5130c630cf53ff13d1c8039fb81c5480b
beatriz <bia.estevam.25@usp.br>
2023-07-06 09:09:39 +0200

    update extractor

--- scripts/extracting_introns/extractor.py
+++ scripts/extracting_introns/extractor.py
@@ -1,19 +1,23 @@
-from Bio import SeqIO
-from Bio.Seq import Seq
-import pyranges as pr
-import argparse
-import pandas as pd
+# script applied to extract intron + flanking region sequences from genome file
+# and gtf file using pyranges library
+# gff files need to be converted into gft using for exemple gtf read
+# It extracts introns/splice sites flanking reagions based on IMEter, spliceator and splice2deep requirements
+# intron size is between 100-150bp
 
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--gtf_input", type=str, required=True)
 parser.add_argument("-f", "--path_to_fasta", type=str, required=True)
-parser.add_argument("-o", "--output", type=str, required=True)
+parser.add_argument("-o", "--output", type=str, required=True,
+                    help='for -o XXXX, output file would be XXXX_[donor/acceptor]_intron.fa')
 parser.add_argument("-c", "--contig_table", type=str, required=True,
-                    help='table with contig IDs and their lengths')
+                    help='table with contig IDs and their lengths - generated with get_contig_table.py')
 parser.add_argument("-t", "--trimming_type", choices=['IMEter', 'spliceator',
-                    'splice2deep'], help='', default='IMEter')
-# parser.add_argument("-x", "--fasta_index", type=str, required=True)
+                    'splice2deep'], help='Options: IMEter SSXXXXXXSS, spliceator 200XSSXXXXXXX200X,\
+                         splice2deep for donor and acceptor 300SXS300X. SS = splice site X = nucleotides\
+                        ', default='IMEter', required=True)
+parser.add_argument("-b", "--flanking_size", type=int,
+                    help='Set specific flanking regions. Defaults: IMEter = 0, Spliceator = 200, Splice2Deep = 300', default=None, required=False)
 args = parser.parse_args()
 
 # fasta = '/home/bia/sugarcane_introns/old_nov30/data/Genomes/\
@@ -29,6 +33,8 @@ contig_lengths = dict(zip(contig_table_pd[0], contig_table_pd[1]))
 
 gtf = pr.read_gtf(args.gtf_input)
 
+flanking_size_dict = {'IMEter': 0, 'spliceator': 200, 'splice2deep': 300}
+
 # Introns features pyranges and its basic operations
 introns = gtf.features.introns(by="transcript")
 # print(introns)
@@ -41,8 +47,8 @@ introns = (introns[(introns.End - introns.Start) < 151])
 # print(introns)
 
 filename = args.output
-filename_donor = filename + "donor"
-filename_acceptor = filename + "acceptor"
+filename_donor = filename + "_donor"
+filename_acceptor = filename + "_acceptor"
 
 trimming_type = args.trimming_type
 
@@ -61,13 +67,18 @@ with open(f'{filename}_intron.fa', 'w') as intron_file:
         if trimming_type == "splice2deep":
             with open(f'{filename_donor}_intron.fa', 'w') as donor_intron_file:
                 with open(f'{filename_acceptor}_intron.fa', 'w') as acceptor_intron_file:
+                    if args.flanking_size is not None:
+                        flanking_size = args.flanking_size
+                    else:
+                        flanking_size = flanking_size_dict[trimming_type]
+
                     # start = Start - 300
-                    donor_start = Start - 300
-                    acceptor_start = End - 302
+                    donor_start = Start - flanking_size
+                    acceptor_start = End - flanking_size + 2
 
                     # end = End + 300
-                    donor_end = Start + 302
-                    acceptor_end = End + 300
+                    donor_end = Start + flanking_size + 2
+                    acceptor_end = End + flanking_size
 
                     if donor_start <= 0 or acceptor_start <= 0:
                         # start = 1
@@ -110,8 +121,14 @@ with open(f'{filename}_intron.fa', 'w') as intron_file:
                             #print(f"\n{id_full} \n{donor_seq}\n{acceptor_seq}\n")
         elif trimming_type == "IMEter":
             # start = Start - 300
-            intron_start = Start
-            intron_end = End
+
+            if args.flanking_size is not None:
+                flanking_size = args.flanking_size
+            else:
+                flanking_size = flanking_size_dict[trimming_type]
+
+            intron_start = Start + flanking_size
+            intron_end = End + flanking_size
 
             if intron_start <= 0:
                 print(
@@ -138,3 +155,36 @@ with open(f'{filename}_intron.fa', 'w') as intron_file:
                     intron_file.write(f'{id_full}\n')
                     intron_file.write(f'{intron_seq}\n')
                     #print(f"\n{id_full} \n{donor_seq}\n{acceptor_seq}\n")
+
+        elif trimming_type == "spliceator":
+
+            if args.flanking_size is not None:
+                flanking_size = args.flanking_size
+            else:
+                flanking_size = flanking_size_dict[trimming_type]
+
+            start = Start - flanking_size
+            end = End + flanking_size
+
+            if start <= 0:
+                start = 1
+                print(id_full)
+
+            if end >= contig_lengths[seq_id]:
+                end = contig_lengths[seq_id]
+                print(id_full)
+
+            intron_seq = seq_full[start:end]
+
+            if strand == "+":
+                intron_seq = intron_seq
+            elif strand == "-":
+                seq = Seq(intron_seq)
+                intron_seq = seq.reverse_complement()
+            else:
+                print(
+                    f'Strand {strand} is not + or - (gene {gene_id} and\
+                        seq_id {seq_id}')
+
+            intron_file.write(f'{id_full}\n')
+            intron_file.write(f'{intron_seq}\n')
